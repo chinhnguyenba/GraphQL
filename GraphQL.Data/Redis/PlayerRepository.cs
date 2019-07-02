839656b17f9f4240b9b6d1b7eb72ad7bbda90583
chinhnguyenba <chinhnguyenba@channelvn.net>
2019-07-02 21:19:45 +0700

    1

---
+++ GraphQL.Data/Redis/PlayerRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.Core.Data;
+using GraphQL.Core.Models;
+
+namespace GraphQL.Data.Redis
+{
+    public class PlayerRepository:IPlayerRepository
+    {
+        public Task<Player> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> GetRandom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Player>> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponceData<Player>> Paging(int num, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> Add(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
